9585519199873d1f697a889602f34101981e6979
Muhammad Mustapha Sesay <muhammadsesay8@gmail.com>
2022-04-18 03:39:28 +0000

    Add: realtime-update on created wave

--- src/App.js
+++ src/App.js
@@ -39,7 +39,9 @@ export default function App() {
           let count = await wavePortalContract.getTotalWaves();
           console.log("Retrieved total wave count...", count.toNumber());
 
-          const waveTxn = await wavePortalContract.wave(inputMessage);
+          const waveTxn = await wavePortalContract.wave(inputMessage, {
+            gasLimit: 300000,
+          });
           console.log("Waved!", waveTxn.hash);
 
           const inProgress = await waveTxn.wait();
@@ -172,6 +174,43 @@ export default function App() {
     setTimeout(() => {
       setInfo("");
     }, 5000);
+
+    let wavePortalContract;
+
+    const onNewWave = (from, timestamp, message) => {
+      console.log("NewWave", from, timestamp, message);
+      setAllWaves((prevState) => [
+        ...prevState,
+        {
+          address: from,
+          timestamp: new Date(timestamp * 1000),
+          message: message,
+        },
+      ]);
+
+      // sort the waves by timestamp so that newest waves are at the top
+      allWaves.sort(
+        (a, b) => new Date(b.timestamp * 1000) - new Date(a.timestamp * 1000)
+      );
+    };
+
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      wavePortalContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+      wavePortalContract.on("NewWave", onNewWave);
+    }
+
+    return () => {
+      if (wavePortalContract) {
+        wavePortalContract.off("NewWave", onNewWave);
+      }
+    };
   }, [info]);
 
   return (
